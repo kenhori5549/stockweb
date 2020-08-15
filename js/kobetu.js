f94916818272bd18be09de6aa52666bd99f2d396
kentohorimoto <kentojason@gmail.com>
2020-08-15 21:30:47 +0900

    first commit

---
+++ js/kobetu.js
@@ -0,0 +1,99 @@
+//var csvfile = 'stock_fundamentals.csv';
+
+function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+//alert(getParam("qcode"))
+qcode=getParam("qcode")
+$(document).ready(function(){
+    $("p").append(qcode)  //そのまま書いたらapendされなかったので、document.readyの後に入れる
+  });
+
+
+var csvfile = '' + qcode + 'h.csv';　//S3のオブジェクト名を格納すること
+function readCsv(data) {
+    var target = '#csv-body';
+    var csv = $.csv.toArrays(data);
+    console.log(csv)
+    var insert = '';
+    $(csv).each(function(i) {
+        if (this.length > 1){ //改行コードのみ入っている場合を除外
+            insert =[]
+            if(i==0){
+                insert +="<thead class='thead'>"
+            }
+            insert += '<tr>';
+         //   arr =this
+            console.log('this',this.length,this)
+
+
+        //for (i=0;i<10;i++){
+         //       if(i =3){
+        //            console.log(arr[i])
+        //            insert +='<td class ="ma25">'+ arr[i]+'</td>';
+        //        }
+        //        else{
+         //           insert +='<td>'+ arr[i]+'</td>';
+         //       }
+        $(this).each(function(i) {
+            if(i==0){
+                insert += '<td class="rowhead">' + this + '</td>';
+            }else
+            if(i==11){
+                if(this =="diviation_rate_25d"){　//表頭が数値化しないように
+                    insert += '<td>' + this + '</td>';
+                }else
+                if(Math.abs(parseFloat(this))<= 0.03){
+                    insert += '<td class="diviation_rate_25d important">' + Math.round(this*100*100)/100 + '%</td>'; 
+                }else{
+                    insert += '<td class="diviation_rate_25d">' + Math.round(this*100*100)/100 + '%</td>';
+                }
+            }else
+            if(i==12){
+                if(this =="diviation_rate_75d"){　//表頭が数値化しないように
+                    insert += '<td>' + this + '%</td>';
+                }else
+                if(Math.abs(parseFloat(this))<= 0.03){
+                    insert += '<td class="diviation_rate_75d important">' + Math.round(this*100*100)/100 + '%</td>';  
+                }else{
+                    insert += '<td class ="diviation_rate_75d">' + Math.round(this*100*100)/100 + '%</td>'; 
+                }
+                    
+            }else
+            if(i==7){
+                if(this =="Change_ratio%"){　//表頭が数値化しないように
+                    insert += '<td>' + this + '</td>';
+                }else
+                //正負判定
+                if(Math.sign(parseFloat(this)) == 1){
+                    insert += '<td class="positive">' + this + '%</td>';
+                }else{
+                    insert += '<td class="negative">' + this + '%</td>';
+
+                }
+            }else{
+                insert += '<td>' + this + '</td>';
+            }  
+        });            
+        insert += '</tr>';
+        if(i==0){
+            insert +="</thead><tbody class='tbody'>"
+        }
+        $(target).append(insert);    
+    }
+    $(target).append("</tbody>");
+});
+   
+}
+
+$(function(){
+    //コールバック関数のきれいな使い方だ・・
+    $.get(csvfile, readCsv, 'text');
+});
---
+++ js/main.js
@@ -0,0 +1,111 @@
+function readCsv(data) {
+    var target = '#csv-body';
+    var csv = $.csv.toArrays(data);
+    console.log(csv)
+    var insert = '';
+    $(csv).each(function(i) {
+        if (this.length > 1){ //改行コードのみ入っている場合を除外
+            insert =[]
+            if(i==0){
+                insert +="<thead class='thead'>"
+            }
+            insert += '<tr>';
+            arr =this
+            console.log('this',this.length,this)
+
+
+        //for (i=0;i<10;i++){
+         //       if(i =3){
+        //            console.log(arr[i])
+        //            insert +='<td class ="ma25">'+ arr[i]+'</td>';
+        //        }
+        //        else{
+         //           insert +='<td>'+ arr[i]+'</td>';
+         //       }
+        $(this).each(function(j) {
+            if(j==0){
+                insert += '<td class="rowhead">' + this + '</td>';
+            }else
+            if(j==3){
+                var qcode =csv[i][0]
+                var kobetu_ja_url ='./stock_kobetu_ja.html?qcode='+qcode
+                
+
+                if(Math.abs(parseFloat(this))<= 4){
+                    insert += '<td class="ma_25s important"><a href=' + kobetu_ja_url+ '>' + this + '</a></td>';
+                    //console.log("25ma important",parseFloat(this)); 
+                }else{
+                    insert += '<td class="ma_25s"><a href=' + kobetu_ja_url+ '>' + this + '</a></td>';
+                    //console.log("25ma important",parseFloat(this));
+                }
+
+            }else
+            if(j==7){
+                //正負判定
+                if(Math.sign(parseFloat(this)) == 1){
+                    if(20 <= parseFloat(this)){
+                        insert += '<td class="op_averages important">' + this + '%</td>';
+                       // console.log("op_averages important",parseFloat(this)); //最後の一文字をとって数値型に変換
+                    }else
+                    if(10 <= parseFloat(this)){
+                        insert += '<td class="op_averages good">' + this + '%</td>';
+                       // console.log("op_averages good",parseFloat(this)); //最後の一文字をとって数値型に変換
+                    }else{
+                        insert += '<td class="op_averages">' + this + '%</td>';
+                        //console.log("op_averages",parseFloat(this)); //最初の一文字をとって数値型に変換
+                    }
+                }else{
+                    insert += '<td class="op_averages">' + this + '%</td>';
+                    //console.log("op_averages",parseFloat(this)); //最初の一文字をとって数値型に変換
+                    //console.log("aaa")
+                }
+                    
+            }else
+            if(j==8){
+                //正負判定
+                if(Math.sign(parseFloat(this)) == 1){
+                    if(20 <= parseFloat(this)){
+                        insert += '<td class="eps important">' + this + '%</td>';
+                        console.log("eps important",parseFloat(this)); //最後の一文字をとって数値型に変換
+                    }else
+                    if(10 <= parseFloat(this)){
+                        insert += '<td class="eps good">' + this + '%</td>';
+                       // console.log("op_averages good",parseFloat(this)); //最後の一文字をとって数値型に変換
+                    }else{
+                        insert += '<td class="eps">' + this + '%</td>';
+                        //console.log("op_averages",parseFloat(this)); //最初の一文字をとって数値型に変換
+                    }
+                }else{
+                    insert += '<td class="eps">' + this + '%</td>';
+                    //console.log("op_averages",parseFloat(this)); //最初の一文字をとって数値型に変換
+                    //console.log("aaa")
+                }
+            }else{
+                insert += '<td>' + this + '</td>';
+            }  
+        });            
+        insert += '</tr>';
+        if(i==0){
+            insert +="</thead><tbody class='tbody'>"
+        }
+        $(target).append(insert);    
+    }
+    $(target).append("</tbody>");
+});
+   
+//stock_kobetu_ja.htmlへのリンク作成
+//$(".ma_25s").each(function(i, d){
+//    $(".ma_25s").append("<a></a>")
+//
+//    //コンソールにインデックス番号とテキストを出力する
+//    console.log('インデックス番号:' + i + '、テキスト:' + $(d).text());
+//  });
+
+
+//
+}
+
+$(function(){
+    //コールバック関数のきれいな使い方だ・・
+    $.get(csvfile, readCsv, 'text');
+});
